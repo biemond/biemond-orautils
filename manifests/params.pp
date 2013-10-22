287a2404cb6b00a5e49fb5dec0c85812e8a96ce4
Edwin Biemond <biemond@gmail.com>
2013-10-22 15:51:48 +0200

    extra parameters options for orautils

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,89 +1,81 @@
 class orautils::params {
 
-  $osOracleHome = $::hostname ? { 
-                                    devagent1  => "/opt/oracle/wls",
-                                    devagent10 => "/opt/oracle/wls",
-                                    devagent30 => "/opt/oracle/wls",
-                                    wls12      => "/oracle/product",
-                                    default    => "/opt/wls", 
+  $osOracleHome = $::hostname ? {
+                                    wls12           => "/oracle/product",
+                                    soabeta2        => "/opt/oracle/wls",
+                                    oimapp          => "/opt/oracle",
+                                    default         => "/opt/wls",
                                 }
 
-  $oraInventory = $::hostname ? { 
-                                    devagent1  => "/opt/oracle/oraInventory",
-                                    devagent10 => "/opt/oracle/oraInventory",
-                                    devagent30 => "/opt/oracle/oraInventory",
-                                    wls12      => "/oracle/oraInventory",
-                                    default    => "/opt/wls/oraInventory", 
+  $oraInventory = $::hostname ? {
+                                    soabeta2        => "/home/oracle/soabetainv",
+                                    wls12           => "/oracle/oraInventory",
+                                    oimapp          => "/opt/oracle/oraInventory",
+                                    default         => "/opt/wls/oraInventory",
                                 }
-
-
 
   $osDomainType = $::hostname ? {
-                                    devagent30 => "soa",
-                                    devagent31 => "soa",
-                                    wls12      => "admin",
-                                    default    => "web", 
+                                    devagent31      => "soa",
+                                    wls12           => "admin",
+                                    oimapp          => "oim",
+                                    default         => "web",
                                 }
 
-  $osLogFolder = $::hostname ? {
-                                      default    => "/data/logs", 
-                               }
+  $osLogFolder = $::hostname ? { default    => "/data/logs", }
 
 
-  $osDownloadFolder = $::hostname ? {
-  	                                  devagent1  => "/data/install/oracle", 
-                                      default    => "/data/install", 
-                                     }
+  $osDownloadFolder = $::hostname ? { default    => "/data/install",}
 
 	$shell        = $::operatingsystem ? { Solaris => "!/usr/bin/ksh",
   															         default => "!/bin/sh",
-  															       }   
+  															       }
 
-  $osMdwHome     = $::hostname ?  { wls12    => "${osOracleHome}/Middleware12c",
-                                    default  => "${osOracleHome}/Middleware11gR1",
-                                  }   
+  $osMdwHome     = $::hostname ?  { wls12           => "${osOracleHome}/Middleware12c",
+                                    default         => "${osOracleHome}/Middleware11gR1",
+                                  }
 
-  $osWlHome     = $::hostname ?  { wls12    => "${osOracleHome}/Middleware12c/wlserver",
-                                   default  => "${osOracleHome}/Middleware11gR1/wlserver_10.3",
-                                 }   
+  $osWlHome     = $::hostname ?  { wls12           => "${osOracleHome}/Middleware12c/wlserver",
+                                   default         => "${osOracleHome}/Middleware11gR1/wlserver_10.3",
+                                 }
 
   $oraUser      = $::hostname ? { default => "oracle", }
 
   $userHome     = $::operatingsystem ? { Solaris => "/export/home",
-  															         default => "/home", 
+  															         default => "/home",
   															       }
+
   $oraInstHome  = $::operatingsystem ? { Solaris => "/var/opt",
-  															         default => "/etc", 
+  															         default => "/etc",
   															       }
 
-  $osDomain     = $::hostname ? {   wls12      => "Wls12c",
-                                    default    => "osbSoaDomain", 
-                                }
-                                
-  $osDomainPath = $::hostname ? { 
-                                    default    => "${osMdwHome}/user_projects/domains/${osDomain}", 
+  $osDomain     = $::hostname ? {   wls12           => "Wls12c",
+                                    oimapp          => "oimDomain",
+                                    default         => "osbSoaDomain",
                                 }
 
-  $nodeMgrPath = $::hostname ? { 
-                                    wls12      => "${osMdwHome}/user_projects/domains/${osDomain}/bin", 
-                                    default    => "${osWlHome}/server/bin", 
+  $osDomainPath = $::hostname ? {
+                                    default    => "${osMdwHome}/user_projects/domains/${osDomain}",
                                 }
 
+  $nodeMgrPath = $::hostname ?  {
+                                    wls12           => "${osMdwHome}/user_projects/domains/${osDomain}/bin",
+                                    default         => "${osWlHome}/server/bin",
+                                }
 
-  $nodeMgrPort = $::hostname ?  { 
-                                    default    => "5556", 
-                                }                                 
+  $nodeMgrPort = $::hostname ?  {
+                                    default    => "5556",
+                                }
 
-  $wlsUser     = $::hostname ?  { 
-                                    default    => "weblogic", 
-                                }                                 
+  $wlsUser     = $::hostname ?  {
+                                    default    => "weblogic",
+                                }
 
-  $wlsPassword = $::hostname ?  { 
-                                    default    => "weblogic1", 
-                                }       
+  $wlsPassword = $::hostname ?  {
+                                    default    => "weblogic1",
+                                }
 
-  $wlsAdminServer = $::hostname ?  { 
-                                    default    => "AdminServer", 
-                                }       
+  $wlsAdminServer = $::hostname ?  {
+                                    default    => "AdminServer",
+                                }
 
 }
